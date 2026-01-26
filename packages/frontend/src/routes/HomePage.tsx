cb91326fc5ae2dbc90af50aba990a576dbb09434
ThanhThang5722 <23521427@gm.uit.edu.vn>
2026-01-26 23:47:27 +0700

    feat: add map component

--- packages/frontend/src/routes/HomePage.tsx
+++ packages/frontend/src/routes/HomePage.tsx
@@ -1,9 +1,11 @@
+import MapView from '@/components/MapView';
 import { Link } from 'react-router';
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-primary-900 to-primary-700 flex items-center justify-center">
       <div className="text-center">
+        <MapView />
         <h1 className="text-5xl font-bold text-white mb-4">
           UIT Volunteer Map
         </h1>
