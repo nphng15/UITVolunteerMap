1b8bb58ec7c27b05c5a80221590d8450c2e16728
Tran Thi Ngoc Xuyen <24522065@gm.uit.du.vn>
2026-02-03 01:06:26 +0700

    refactor: add logo for public header

--- packages/frontend/src/components/layouts/PublicHeader.tsx
+++ packages/frontend/src/components/layouts/PublicHeader.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router";
+import uitLogo from "@/assets/icons/uitvolunteer.svg";
 
 export default function PublicHeader() {
   return (
-    <header className="w-full bg-blue-500 text-white">
+ <header className="w-full bg-blue-500 text-white">
       <div className="max-w-6xl mx-auto h-14 px-6 flex items-center justify-between">
-        <div className="flex items-center gap-2 font-bold text-sm">
-          <span>✉</span>
-          <span>UITVolunteer</span>
+        <div className="flex items-center gap-2">
+          <img
+            src={uitLogo}
+            alt="UITVolunteer"
+            className="h-6 w-auto"
+          />
         </div>
 
         <Link
